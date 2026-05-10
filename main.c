c99d373b4ba74cc376e3ef6624cc64ab31f04a16
Dantallope <dantebraccialini@gmail.com>
2026-05-09 22:48:15 -0700

    added readLine function

--- main.c
+++ main.c
@@ -8,6 +8,10 @@
 #define PORT "3456"
 #define BUFFER_SIZE 4096
 
+void readLine (int fd, char *buffer, int size);
+void newLineTrim (char *str);
+int connectToServer(const char *hostname);
+
 int main(void){
 
     int choice;
@@ -15,8 +19,8 @@ int main(void){
     char hostname[100];
 
     printf("Choose a server:\n");
-    printf("1. richmond.cs.sierracollege.edu\n");
-    printf("2. london.cs.sierracollege.edu\n");
+    printf("1. Richmond\n");
+    printf("2. London\n");
     printf("Choice: ");
     scanf("%d", &serverChoice);
     getchar();
@@ -29,4 +33,43 @@ int main(void){
         printf("Invalid server choice.\n");
         return 1;
     }
+
+    int fd = connectToServer(hostname);
+
+    if (fd == -1){
+        printf("Couldn't connect to server.\n");
+        return 1;
+    }
+
+    char greeting[BUFFER_SIZE];
+    readLine(fd, greeting, BUFFER_SIZE);
+    printf("Connected: %s\n", greeting);
+}
+
+void readLine (int fd, char *buffer, int size){
+    int i = 0;
+    char ch;
+
+    while (i < size - 1){
+        int readBytes = read(fd, &ch, 1);
+        
+        if (readBytes <= 0 ){
+            break;
+        }
+
+        buffer[1] = ch;
+        i++;
+
+        if (ch == '\n'){
+            break;
+        }
+    }
+
+    buffer[i] = '\0';
+
+    //trimline func
+}
+
+int connectToServer(const char *hostname){
+    return 0;
 }
