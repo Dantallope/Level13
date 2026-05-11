f721cdfa229c3e34a15bc52c5557c259301f3b78
Dantallope <dantebraccialini@gmail.com>
2026-05-10 20:00:44 -0700

    Download File Function working download speed is not working

--- main.c
+++ main.c
@@ -13,6 +13,8 @@ void newLineTrim (char *str);
 int connectToServer(const char *hostname);
 void sendCommand(int fd, const char *command);
 void listFiles(int fd);
+void downloadFile(int fd, const char *filename);
+long getFileSize(int fd, const char *filename);
 
 int main(void){
 
@@ -61,7 +63,13 @@ int main(void){
     if (choice2 == 'L'){
         listFiles(fd);
     }else if (choice2 == 'D'){
-        //downloadfile
+        char filename[256];
+
+        printf("Enter filename for download: ");
+        fgets(filename, sizeof(filename),stdin);
+        newLineTrim(filename);
+        downloadFile(fd, filename);
+        
     }else if (choice2 == 'Q'){
         //quit
     }else{
@@ -126,10 +134,10 @@ void listFiles(int fd){
     sendCommand(fd,"LIST\n");
     readLine(fd, line, BUFFER_SIZE);
 
-    // if (strncmp(line, "+OK", 3) != 0){
-    //     printf("Server error: %s\n",line);
-    //     return;
-    // }
+    if (strncmp(line, "+OK", 3) != 0){
+        printf("Server error: %s\n",line);
+        return;
+    }
 
     printf("\nAvailable files:\n");
     printf("%-15s %s\n", "Size", "Filename");
@@ -145,3 +153,99 @@ void listFiles(int fd){
 
 
 }
+
+long getFileSize(int fd, const char *filename){
+    char command[512];
+    char response[BUFFER_SIZE];
+    long size;
+
+    snprintf(command, sizeof(command), "SIZE %s\n", filename);
+    sendCommand(fd, command);
+
+    readLine(fd, response, BUFFER_SIZE);
+
+    if (strncmp(response, "+OK", 3) != 0){
+        printf("Server error: %s\n", response);
+        return -1;
+    }
+
+    if (sscanf(response, "+OK %ld", &size) != 1){
+        printf("Couldn't read file size.\n");
+        return -1;
+    }
+    return size;
+}
+
+void downloadFile(int fd, const char *filename){
+    char command[512];
+    char response[BUFFER_SIZE];
+    char buffer[BUFFER_SIZE];
+
+    long fileSize = getFileSize(fd, filename);
+
+    if (fileSize < 0){
+        return;
+    }
+
+    snprintf(command, sizeof(command), "GET %s\n", filename);
+    sendCommand(fd, command);
+
+    readLine(fd, response, BUFFER_SIZE);
+
+    if (strncmp(response, "+OK", 3) != 0) {
+        printf("Server error: %s\n", response);
+        return;
+    }
+
+    FILE *out = fopen(filename, "wb");
+
+    if (out == NULL){
+        printf("Couldn't open file for writing.\n");
+        return;
+    }
+
+    struct timeval start;
+    struct timeval end;
+
+    gettimeofday(&start, NULL);
+
+    long totalRead = 0;
+
+    while (totalRead < fileSize){
+        long remaining = fileSize - totalRead;
+        int toRead;
+
+        if (remaining < BUFFER_SIZE){
+            toRead = remaining;
+        }else{
+            toRead = BUFFER_SIZE;
+        }
+
+        int bytesRead = read(fd, buffer, toRead);
+
+        if (bytesRead <= 0){
+            printf("Connection Lost :[");
+            break;
+        }
+
+        fwrite(buffer, 1, bytesRead, out);
+        totalRead += bytesRead;
+    }
+
+    gettimeofday(&end,NULL);
+    fclose(out);
+    
+    if (totalRead == fileSize){
+        double elapsed = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000;
+
+        double kb = fileSize / 1024;
+        double speed = kb / elapsed;
+
+        printf("Downloaded %s successfully.\n", filename);
+        printf("Size: %.2f KB\n", kb);
+        printf("Speed: %.2f KB/sec\n", speed);
+    }else{
+        printf("Download Failed");
+    }
+}
+
